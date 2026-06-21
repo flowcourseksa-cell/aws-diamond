8781abc2f231d51cc41a63a421002e377ff7a650
Mohamed Omar <moomarnasr1@gmail.com>
2026-06-21 02:35:44 +0000

    chore: حذف سكربتات مؤقتة وخطيرة من جذر المستودع

--- test_activations.ts
+++
@@ -1,6 +0,0 @@
-import { fetchPendingActivations } from "./lib/supabase/services/activations";
-async function test() {
-  const data = await fetchPendingActivations();
-  console.log(JSON.stringify(data, null, 2));
-}
-test();
